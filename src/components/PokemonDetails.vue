bca7169aee8985235657e950bf4b2f152a92d32b
calulazone <lutinelo.lg@gmail.com>
2024-11-26 16:02:39 +0100

    set router

--- src/components/PokemonDetails.vue
+++ src/components/PokemonDetails.vue
@@ -4,6 +4,10 @@ import { useRoute } from 'vue-router'
 const route = useRoute()
 const pokemonId = route.params.id.toString()
 console.log(pokemonId)
+
+//Récupérer le pokemon associé
+//const pokemon = pokemonStore.getElementById(pokemonId)
+
 </script>
 
 <template>
